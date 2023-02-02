c0505f80749dc3c671177fd7f91ba3265df3ea92
Leon Berenschot <leon@wendbaar.nl>
2023-02-02 11:50:23 +0100

    npm i -D @vicons/utils  @vicons/carbon

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import "vfonts/Lato.css";
+import "vfonts/FiraCode.css";
+
 import App from "./App.vue";
 import router from "./router";
 
