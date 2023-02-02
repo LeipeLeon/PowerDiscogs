8182383b9e27237cadc1478f606dba13fc841dce
Leon Berenschot <leon@wendbaar.nl>
2023-02-02 14:07:40 +0100

    show found details in sidebar

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -6,16 +6,18 @@ import {
   NLayoutContent,
   NLayoutSider,
   NLayoutFooter,
-  NH2,
   NInput,
   useMessage,
-  NMessageProvider,
+  NSwitch,
 } from "naive-ui";
 
 import { ref } from "vue";
 
 const inputString = ref(null);
-const masterItems = ref([]);
+const masterItems = ref([{ id: -1, title: "First do a search" }]);
+const releaseItems = ref([{ id: -1, title: "No master release selected" }]);
+const inverted = ref(false);
+
 const message = useMessage();
 const handleKeyUp = (e) => {
   if (e.key === "Enter") {
@@ -24,7 +26,11 @@ const handleKeyUp = (e) => {
     message.info("[Event keyup]");
   }
 };
-
+const handleMasterClick = (e) => {
+  e.preventDefault();
+  console.log("handleMasterClick", e.target.dataset.id);
+  searchReleaseItems(e.target.dataset.id);
+};
 // Search Master Release
 const searchMasterRelease = () => {
   const fetchUrl =
@@ -41,70 +47,93 @@ const searchMasterRelease = () => {
     .then((res) => res.json())
     .then((data) => (masterItems.value = data.results))
     .catch(console.error.bind(console));
+  console.log("masterItems", masterItems);
+};
+// Search Master Release
+const searchReleaseItems = (masterId) => {
+  const fetchUrl = `https://api.discogs.com/masters/${masterId}/versions?format=7%22`;
+
+  fetch(fetchUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Discogs token=${import.meta.env.VITE_DISCOGS_API_TOKEN}`,
+      "User-Agent": "DiscogsRapidSearcher/0.1 +https://wendbaar.nl",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => (releaseItems.value = data.versions))
+    .catch(console.error.bind(console));
+  console.log("releaseItems", releaseItems);
 };
 </script>
 
 <template>
   <div style="height: 100vh; position: relative">
     <n-layout position="absolute">
-      <n-layout-header style="height: 64px; padding: 24px" bordered>
+      <n-layout-header
+        style="height: 64px; padding: 24px"
+        bordered
+        :inverted="inverted"
+      >
         <n-input
           v-model:value="inputString"
           type="text"
-          placeholder="Basic Input"
+          placeholder="Search for artist / Title"
           autofocus
           @keyup="handleKeyUp"
         />
       </n-layout-header>
       <n-layout has-sider position="absolute" style="top: 64px; bottom: 64px">
-        <n-layout-sider bordered content-style="padding: 24px;">
-          <ul>
-            <li v-for="item in masterItems">{{ item }}</li>
-          </ul>
-          <!-- <n-input
-            v-model:value="inputString"
-            type="textarea"
-            placeholder="Basic Textarea"
-          /> -->
+        <n-layout-sider
+          bordered
+          content-style="padding: 24px;"
+          :inverted="inverted"
+          width="500"
+        >
+          <div
+            v-for="item in masterItems"
+            :key="item.id"
+            class="master-item"
+            @click="handleMasterClick"
+            :data-id="item.id"
+          >
+            <img
+              :src="item.thumb"
+              style="max-height: 50px; vertical-align: middle"
+            />
+            {{ item.title }}
+          </div>
         </n-layout-sider>
-        <n-layout content-style="padding: 24px;">
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-          <n-h2>Pingshan Road</n-h2>
-        </n-layout>
+        <n-layout-content content-style="padding: 24px;">
+          <div
+            v-for="item in releaseItems"
+            :key="item.id"
+            class="release-item"
+            :data-id="item.id"
+          >
+            <img
+              :src="item.thumb"
+              style="max-height: 50px; vertical-align: middle"
+            />
+            {{ item.title }}
+          </div>
+        </n-layout-content>
       </n-layout>
       <n-layout-footer
         bordered
         position="absolute"
         style="height: 64px; padding: 24px"
+        :inverted="inverted"
       >
-        城府路
+        <n-space> <n-switch v-model:value="inverted" /> inverted </n-space>
       </n-layout-footer>
     </n-layout>
   </div>
 </template>
 
 <style scoped>
-.n-layout-header,
-.n-layout-footer {
-  background: rgba(128, 128, 128, 0.2);
-  padding: 24px;
-}
-
-.n-layout-sider {
-  background: rgba(128, 128, 128, 0.3);
-}
-
-.n-layout-content {
-  background: rgba(128, 128, 128, 0.4);
+.release-item:nth-child(even),
+.master-item:nth-child(even) {
+  background: #f1f1f1;
 }
 </style>
