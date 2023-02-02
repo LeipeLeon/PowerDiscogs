a10a1480d387d4e7c13fa6795c2f3ff8af3f39e5
Leon Berenschot <leon@wendbaar.nl>
2023-02-02 22:50:54 +0100

    add watermark

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import { NMessageProvider } from "naive-ui";
+// import { RouterLink, RouterView } from "vue-router";
+import { NMessageProvider, NWatermark } from "naive-ui";
 
 import Container from "./components/Container.vue";
 </script>
@@ -9,4 +9,17 @@ import Container from "./components/Container.vue";
   <n-message-provider>
     <container />
   </n-message-provider>
+  <n-watermark
+    content="PROOF OF CONCEPT"
+    cross
+    fullscreen
+    :font-size="16"
+    :line-height="16"
+    :width="384"
+    :height="384"
+    :x-offset="12"
+    :y-offset="80"
+    :rotate="-15"
+    font-color="rgba(128, 0, 0, 0.3)"
+  />
 </template>
